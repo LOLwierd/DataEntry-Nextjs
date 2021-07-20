26eeb3630b142c3a6112d94a50836d46531cbdc6
Ayaan Retiwala <ayaan.180410107098@gmail.com>
2021-07-20 23:07:44 +0530

    Printing is shit

--- pages/students/[id].tsx
+++ pages/students/[id].tsx
@@ -1,19 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useContext } from "react";
+import Context from "../../utils/Context";
 import { Report } from "../../types";
 import { GetServerSideProps } from "next";
 import axios from "axios";
 import { getRoman, getShortName } from "../../utils";
 import EmptyComponent from "../../components/EmptyComponent";
 import { useRouter } from "next/router";
+import { getFontDefinitionFromNetwork } from "next/dist/next-server/server/font-utils";
 
 export default function StudentIndi({ report }: { report: Report }) {
   let course: string;
   const { query } = useRouter();
+  const { setError } = useContext(Context);
+
   if (report) {
     course = getShortName(report.course.course);
   }
   useEffect(() => {
-    if (query.print) window.print();
+    if (query.print) {
+      if (report) window.print();
+      else setError("Result not found!");
+    }
   }, []);
 
   // function to print html using window.print()
@@ -24,8 +31,8 @@ export default function StudentIndi({ report }: { report: Report }) {
     <main>
       {report ? (
         <>
-          <button id="print-btn" onClick={printHTML}>
-            Print window
+          <button id="print-btn" className="primary" onClick={printHTML}>
+            Print
           </button>
           <table id="transcript">
             <thead>
@@ -94,6 +101,85 @@ export default function StudentIndi({ report }: { report: Report }) {
             <tr>
               <td colSpan={8}>CUMULATIVE PERFORMANCE INDEX (CPI)</td>
               <td>{report.Result[report.Result.length - 1].cpi}</td>
+            </tr>
+            <tr>
+              <td
+                colSpan={9}
+                style={{
+                  height: "8em",
+                  background: "white",
+                  borderLeft: "none",
+                  borderRight: "none",
+                  borderBottom: "none",
+                  verticalAlign: "bottom",
+                  position: "relative",
+                }}
+              >
+                <div style={{ position: "absolute", bottom: 0, right: 0 }}>
+                  Seal & Signature of the Principal
+                </div>
+              </td>
+            </tr>
+          </table>
+          <table
+            id="transcript"
+            className="printHide"
+            style={{
+              // width: "100%",
+              margin: "0 auto",
+            }}
+          >
+            <tr className="no-border">
+              <td>PERCENTAGE OF RANGE</td>
+              <td>100</td>
+              <td>90</td>
+              <td>80</td>
+              <td>70</td>
+              <td>65</td>
+              <td>60</td>
+              <td>55</td>
+              <td>50</td>
+              <td>45</td>
+              <td>{"<40"}</td>
+            </tr>
+            <tr className="no-border">
+              <td>EQUIVALENT GRADE</td>
+              <td>A++</td>
+              <td>A+</td>
+              <td>A</td>
+              <td>B+</td>
+              <td>B</td>
+              <td>B-</td>
+              <td>C</td>
+              <td>D</td>
+              <td>E</td>
+              <td>F</td>
+            </tr>
+            <tr className="no-border">
+              <td>SEMESTER PERFORMANCE INDEX (SPI)</td>
+              <td colSpan={10}>Sum (Percentage) / No. of Subjects</td>
+            </tr>
+            <tr className="no-border">
+              <td>CUMULATIVE PERFORMANCE INDEX (CPI)</td>
+              <td colSpan={10}>Sum (SPI per sem ) / No. of Sem</td>
+            </tr>
+            <tr className="no-border">
+              <td>C.P.I.</td>
+              <td></td>
+              <td colSpan={2}>{"> 6.6"}</td>
+              <td colSpan={2}>6.6 - 6</td>
+              <td colSpan={2}>6 - 5</td>
+              <td colSpan={2}>{"< 5"}</td>
+              <td></td>
+            </tr>
+            <tr className="no-border">
+              <td>Performance</td>
+              <td colSpan={1}></td>
+              <td colSpan={2}>Excellent</td>
+              <td colSpan={2}>Very Good</td>
+              <td colSpan={2}>Good</td>
+              <td colSpan={2}>Average</td>
+              <td colSpan={1}></td>
             </tr>
           </table>
         </>
